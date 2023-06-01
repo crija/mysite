7f354b8fa07b35d4e4673326740ef246ca8ac0d3
crija <crijacarla@gmail.com>
2023-05-31 21:03:57 -0300

    Add import Question in file polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from polls.models import People
+from .models import Question
 
 # Register your models here.
 
 admin.site.register(People)
+admin.site.register(Question)

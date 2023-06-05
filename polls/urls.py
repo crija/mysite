12d47636d91f8cf612cacd0c40f8d2bc24a05579
crija <crijacarla@gmail.com>
2023-06-04 21:16:31 -0300

    Add new pages

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,23 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path("<int:question_id>/",
+views.detail, name="detail"),
+
+    path("<int:question_id>/results/",
+views.vote, name="results"),
+
+    path("<int:question_id>/vote/",
+views.vote, name="vote"),
+
     path('criar', views.criar, name='criar'),
+
     path('criar/cadastro', views.cadastro, name='cadastro'),
+
     path('cadastro/recursos', views.recursos, name='recursos'),
+
     path('exer', views.exer, name='exer'),
+
     path('people', views.exer, name='people'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -80,3 +80,14 @@ def exer(request):
 
 </body>
 </html>""")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
